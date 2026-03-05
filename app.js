f80c1a6019d1d0baa2b8b503ddb22ce61cc34d14
23098-code <23098@pua.ac.th>
2026-03-05 23:22:56 +0700

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const URL = "https://23098-code.github.io/corn/";
 let model, webcam;
 
 async function init() {
+
     const modelURL = URL + "model.json";
     const metadataURL = URL + "metadata.json";
 
@@ -12,9 +13,9 @@ async function init() {
     await webcam.setup();
     await webcam.play();
 
-    window.requestAnimationFrame(loop);
-
     document.getElementById("webcam-container").appendChild(webcam.canvas);
+
+    window.requestAnimationFrame(loop);
 }
 
 async function loop() {
@@ -23,13 +24,24 @@ async function loop() {
 }
 
 async function predict() {
+
+    if(!model){
+        document.getElementById("result").innerHTML="โมเดลยังโหลดไม่เสร็จ";
+        return;
+    }
+
     const prediction = await model.predict(webcam.canvas);
 
     let text = "";
+
     for (let i = 0; i < prediction.length; i++) {
+
         text += prediction[i].className + " : " +
         (prediction[i].probability * 100).toFixed(2) + "%<br>";
+
     }
 
     document.getElementById("result").innerHTML = text;
 }
+
+window.onload = init;
